6f3c3853ef29fecfc94aae6ce5a5f39f117d1449
Jo Mendes <jlmendes1996@gmail.com>
2021-12-28 23:46:15 -0500

    copy

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -13,7 +13,7 @@ export default function AboutMe(props) {
     const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
 
     const SCREEN_CONSTSANTS = {
-      description: "I’m a self-taught Software Developer that that has been working in tech for the last 2 years. I’ve had the pleasure of working alongside some incredible entrepreneurs, designers, and developers. I’m extremly interested in writing reuseable codes, collaborating, and reactive Javascript frameworks. From the beginning days of when I first got into software development up until now, I’ve learned and gained a solid understanding of web development standards, the importance of reusability/scalability in large enterprise applications, and how user expereince means everything.",
+      description: "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah .",
       highlights: {
         skills: [
           "React","Javascript ES6+","CSS","HTML","SCSS","SASS","GitHub","Markup", "UI Design", "UI Experience","Google Analytics", "Google Tag Manager", "Enhanced Ecoommerce GA","Tailwinds","Bootstrap", "Wordpress", "Figma", "Adobe Photoshop", "SEO"
