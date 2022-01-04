e978ea97615ed59d28a0957556dc9371b0e4926d
Jo Mendes <jlmendes1996@gmail.com>
2022-01-03 22:38:10 -0500

    bio update

--- server/server.js
+++ server/server.js
@@ -5,20 +5,10 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-   res.sendFile(path.join('https://',publicPath, 'index.html'));
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
 });
 
-var http = require('http');
-var enforce = require('express-sslify');
- 
- 
-// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
-// a load balancer (e.g. Heroku). See further comments below
-app.use(enforce.HTTPS());
- 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+
