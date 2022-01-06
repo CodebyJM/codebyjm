cb0784bbbe0bf9b7a69c3caae55b0e1901b55ef2
Jo Mendes <jlmendes1996@gmail.com>
2022-01-06 17:25:20 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
-const path = require('path');
+var express = require('express');
 var http = require('http');
 var enforce = require('express-sslify');
-var express = require('express');
+ 
 var app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+ 
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku). See further comments below
 app.use(enforce.HTTPS());
  
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
-});
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!');
 });
 
 
